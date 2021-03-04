f738edb6d68376e18d31ee1a63dd0880374e3e5c
benjamin <carlosbpac@gmail.com>
2021-03-03 21:08:16 -0600

    Primeros servlets

---
+++ PROYECTO1_COMPI1/src/main/java/com/carlos/proyecto1_compi1/Servlets/registroUsuario.java
@@ -0,0 +1,21 @@
+package com.carlos.proyecto1_compi1.Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class registroUsuario extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
+}
