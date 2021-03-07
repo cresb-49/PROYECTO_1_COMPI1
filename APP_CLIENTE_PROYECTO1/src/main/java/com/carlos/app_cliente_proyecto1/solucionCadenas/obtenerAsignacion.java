f258de5f66ef0e5a6af9a555914af0cfb183ed88
Sara Pac <carlosbpac@gmail.com>
2021-03-06 19:17:48 -0600

    clase para obtener valores dentro de la asignacion

---
+++ APP_CLIENTE_PROYECTO1/src/main/java/com/carlos/app_cliente_proyecto1/solucionCadenas/obtenerAsignacion.java
@@ -0,0 +1,15 @@
+package com.carlos.app_cliente_proyecto1.solucionCadenas;
+
+public class obtenerAsignacion {
+    public obtenerAsignacion(){
+        
+    }
+    public String getAsignacion(String asignacion){
+        if(asignacion.length()!=0){
+            return asignacion.substring(1, asignacion.length()-1);
+        }
+        else{
+            return null;
+        }
+    }
+}
