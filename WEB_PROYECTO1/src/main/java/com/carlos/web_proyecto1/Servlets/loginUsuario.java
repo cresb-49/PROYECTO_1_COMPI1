303f4713332485d653d7f5ed721c085eecbc90cb
Carlos Pac <carlosbpac@gmail.com>
2021-03-08 15:08:04 -0600

    Librerias de JSON y lectura del envio de parametros

--- WEB_PROYECTO1/src/main/java/com/carlos/web_proyecto1/Servlets/loginUsuario.java
+++ WEB_PROYECTO1/src/main/java/com/carlos/web_proyecto1/Servlets/loginUsuario.java
@@ -4,7 +4,9 @@ import com.carlos.web_proyecto1.Lexer.lexerIndigo;
 import com.carlos.web_proyecto1.Objetos.userNew;
 import com.carlos.web_proyecto1.Objetos.usuario;
 import com.carlos.web_proyecto1.Parser.parserIndigo;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,17 +20,29 @@ public class loginUsuario extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("FUNCIONAMIENTO DEL SERVLET");
+        
         usuario user=new usuario();
-        String indigoCode = req.getParameter("code");
-        System.out.println("Cadena recivida: "+indigoCode);
+        
+        
+        StringBuilder resultado = new StringBuilder();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String linea;
+        
+        while ((linea = rd.readLine()) != null) {
+            resultado.append(linea);
+        }
+        
+        rd.close();
+        
         try {
-            lexerIndigo lex = new lexerIndigo(new StringReader(indigoCode));
+            lexerIndigo lex = new lexerIndigo(new StringReader(resultado.toString()));
             parserIndigo parser = new parserIndigo(lex);
             parser.parse();
             user=parser.getLogUser();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
         System.out.println("Usuario recuperado: "+user.toString());
         
     }
