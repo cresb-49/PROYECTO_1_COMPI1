8770efe777320828a3d16ef21da356dddb6e3479
benjamin <carlosbpac@gmail.com>
2021-03-26 19:30:19 -0600

    Elementos para la exportacion de formularios del sistema

--- WEB_PROYECTO1/src/main/java/com/carlos/web_proyecto1/Acciones/FormularioToJson/FormToJson.java
+++ WEB_PROYECTO1/src/main/java/com/carlos/web_proyecto1/Acciones/FormularioToJson/FormToJson.java
@@ -2,32 +2,150 @@ package com.carlos.web_proyecto1.Acciones.FormularioToJson;
 
 import com.carlos.web_proyecto1.Objetos.Componente;
 import com.carlos.web_proyecto1.Objetos.Formulario;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FormToJson {
-    
+
     public FormToJson() {
     }
-    
+
     public String toJsonString(Formulario form) {
         String code = null;
-        
+
         code = "new.formulario({\n"
                 + "\"ID_FORMULARIO\": \"" + form.getId() + "\" ,\n"
                 + "\"TITULO\": \"" + form.getTitulo() + "\" ,\n"
                 + "\"NOMBRE\": \"" + form.getNombre() + "\" ,\n"
                 + "\"TEMA\": \"" + form.getTema() + "\" ,\n"
-                + "\"ESTRUCTURA\":(";
+                + "\"ESTRUCTURA\":(\n";
         Componente tmp = null;
+        String tmpCode = null;
         for (int i = 0; i < form.getComponentes().size(); i++) {
             tmp = form.getComponentes().get(i);
             
+            switch (tmp.getClase()) {
+                case "CAMPO_TEXTO":
+                    tmpCode = this.campo_texto(tmp);
+                    break;
+                case "AREA_TEXTO":
+                    tmpCode = this.area_texto(tmp);
+                    break;
+                case "CHECKBOX":
+                    tmpCode = this.checkbox(tmp);
+                    break;
+                case "RADIO":
+                    tmpCode = this.radio(tmp);
+                    break;
+                case "FICHERO":
+                    tmpCode = this.fichero(tmp);
+                    break;
+                case "IMAGEN":
+                    tmpCode = this.imagen(tmp);
+                    break;
+                case "COMBO":
+                    tmpCode = this.combo(tmp);
+                    break;
+                case "BOTON":
+                    tmpCode = this.boton(tmp);
+                    break;
+            }
+            code = code + "{\n"+tmpCode+"}";
             
-            
-            
+            if(i==((form.getComponentes().size())-1)){
+                code = code +"\n";
+            }else{
+                code = code +",\n";
+            }
         }
         code = code + ")\n"
                 + "})";
-        
+
+        return code;
+    }
+
+    private String campo_texto(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"NOMBRE_CAMPO\": \"" + comp.getNombre_campo() + "\",\n"
+                + "\"CLASE\": \""+comp.getClase()+"\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String area_texto(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"NOMBRE_CAMPO\": \"" + comp.getNombre_campo() + "\",\n"
+                + "\"CLASE\": \"" + comp.getClase() + "\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"FILAS\": \""+comp.getFilas()+"\",\n"
+                + "\"COLUMNAS\": \""+comp.getColumnas()+"\",\n"
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String checkbox(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"NOMBRE_CAMPO\": \"" + comp.getNombre_campo() + "\",\n"
+                + "\"CLASE\": \"" + comp.getClase() + "\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"OPCIONES\": \""+comp.getOpciones()+"\",\n"
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String radio(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"NOMBRE_CAMPO\": \"" + comp.getNombre_campo() + "\",\n"
+                + "\"CLASE\": \"" + comp.getClase() + "\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"OPCIONES\": \""+comp.getOpciones()+"\",\n"
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String combo(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"NOMBRE_CAMPO\": \"" + comp.getNombre_campo() + "\",\n"
+                + "\"CLASE\": \"" + comp.getClase() + "\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"OPCIONES\": \""+comp.getOpciones()+"\",\n"
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String fichero(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"NOMBRE_CAMPO\": \"" + comp.getNombre_campo() + "\",\n"
+                + "\"CLASE\": \""+comp.getClase()+"\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String imagen(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"CLASE\": \""+comp.getClase()+"\",\n"
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",\n")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",\n")
+                + "\"URL\": \"" + comp.getUrl() + "\",\n"
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"\n";
+        return code;
+    }
+
+    private String boton(Componente comp) {
+        String code = "\"ID\": \"" + comp.getId() + "\",\n"
+                + "\"CLASE\": \""+comp.getClase()+"\","
+                + ((comp.getAlineacion() == null) ? "" : "\"ALINEACION\": \"" + comp.getAlineacion() + "\",")
+                + ((comp.getRequerido() == null) ? "" : "\"REQUERIDO\": \"" + comp.getRequerido() + "\",")
+                + "\"TEXTO_VISIBLE\": \"" + comp.getTexto_visible() + "\"";
         return code;
     }
 }
--- WEB_PROYECTO1/src/main/java/com/carlos/web_proyecto1/Servlets/exportarFormulario.java
+++ WEB_PROYECTO1/src/main/java/com/carlos/web_proyecto1/Servlets/exportarFormulario.java
@@ -1,8 +1,14 @@
-
 package com.carlos.web_proyecto1.Servlets;
 
+import com.carlos.web_proyecto1.Acciones.FormularioToJson.FormToJson;
+import com.carlos.web_proyecto1.DataBases.DBFormularios;
+import com.carlos.web_proyecto1.Objetos.Formulario;
 import com.carlos.web_proyecto1.escribirRespuestaIndigo.resIndigo;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -13,19 +19,54 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/exportForm")
 public class exportarFormulario extends HttpServlet {
+
     private resIndigo respuesta = new resIndigo();
-    
+    private FormToJson conv = new FormToJson();
+    private DBFormularios baseForms = new DBFormularios();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String original = req.getServletContext().getRealPath("");
+        String path = original.replaceAll("/WEB_PROYECTO1/target/WEB_PROYECTO1-1.0-SNAPSHOT/", "");
+        this.cargarBaseDatos(path);
+
         String idForm = req.getParameter("idForm");
         System.out.println("peticion de formulario");
-        if(idForm!=null){
-            envioMensaje(req, resp, "Se recibio con exito el id del formulario");
-        }else{
+        
+        if (idForm != null) {
+            
+            Formulario tmp = this.baseForms.buscarFormulario(idForm);
+            
+            if(tmp==null){
+                envioMensaje(req, resp, "No existe un formulario con el id: "+idForm);
+            }else{
+                envioFormulario(req, resp, conv.toJsonString(tmp));
+            }
+            
+        } else {
             envioMensaje(req, resp, "No se recibio ningun id de formulario");
         }
     }
-    
+
+    private void cargarBaseDatos(String path) {
+        Gson gson = new Gson();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path + "/Almacenamiento/forms.db")));
+            String liena;
+            String code = "";
+
+            while ((liena = br.readLine()) != null) {
+                code += liena;
+            }
+            br.close();
+            this.baseForms = gson.fromJson(code, DBFormularios.class);
+        } catch (Exception e) {
+            System.out.println("Error en carga de base de datos formularios");
+            e.printStackTrace();
+        }
+    }
+
     private void envioMensaje(HttpServletRequest req, HttpServletResponse resp, String mensaje) throws ServletException, IOException {
         try {
             PrintWriter writer = resp.getWriter();
@@ -34,6 +75,18 @@ public class exportarFormulario extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
+
+    private void envioFormulario(HttpServletRequest req, HttpServletResponse resp, String code) throws ServletException, IOException {
+        try {
+            PrintWriter writer = resp.getWriter();
+            writer.println(code);
+            writer.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
